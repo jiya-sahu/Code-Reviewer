5fdf82016d05f3d7d381ecdf1a49ae77f1a6c65d
jiya-sahu <jiyasahu135@gmail.com>
2025-06-08 20:06:10 +0530

    handled cors policy

--- Backened/src/app.js
+++ Backened/src/app.js
@@ -4,7 +4,11 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'https://auto-review.netlify.app/',
+  methods: ['POST'],
+  credentials: true
+}));
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
